86371a5bbf29df36d185273733ef86dc5c34edb6
Philip Chung <philipchung1995@yahoo.com>
2023-02-02 21:50:22 -0800

    Document trait implementations

--- src/lib.rs
+++ src/lib.rs
@@ -151,11 +151,52 @@ impl<T> Infinitable<T> {
 }
 
 impl<T> From<T> for Infinitable<T> {
+    /// Converts from a value `T` to `Finite` containing the underlying value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use infinitable::*;
+    ///
+    /// let finite = Infinitable::from(5);
+    /// assert_eq!(Finite(5), finite);
+    ///
+    /// // Warning: There is no special handling for pre-existing infinite values
+    /// let fp_infinity = Infinitable::from(f32::INFINITY);
+    /// assert_eq!(Finite(f32::INFINITY), fp_infinity);
+    /// assert_ne!(Infinity, fp_infinity);
+    /// ```
+    ///
+    /// # Versioning
+    ///
+    /// Available since 1.2.0.
     fn from(value: T) -> Infinitable<T> {
         Finite(value)
     }
 }
 
+/// Partial order, where the underlying type `T` implements a partial order.
+///
+/// `NegativeInfinity` compares less than all other values,
+/// and `Infinity` compares greater than all other values.
+///
+/// # Examples
+///
+/// ```
+/// use infinitable::*;
+/// use std::cmp::Ordering;
+///
+/// let finite = Finite(5);
+/// let infinity = Infinity;
+/// let negative_infinity = NegativeInfinity;
+///
+/// assert_eq!(Some(Ordering::Less), finite.partial_cmp(&infinity));
+/// assert_eq!(Some(Ordering::Greater), finite.partial_cmp(&negative_infinity));
+/// ```
+///
+/// # Versioning
+///
+/// Available since 1.0.0.
 impl<T> PartialOrd for Infinitable<T>
 where
     T: PartialOrd,
@@ -168,6 +209,28 @@ where
     }
 }
 
+/// Total order, where the underlying type `T` implements a total order.
+///
+/// `NegativeInfinity` compares less than all other values,
+/// and `Infinity` compares greater than all other values.
+///
+/// # Examples
+///
+/// ```
+/// use infinitable::*;
+/// use std::cmp::Ordering;
+///
+/// let finite = Finite(5);
+/// let infinity = Infinity;
+/// let negative_infinity = NegativeInfinity;
+///
+/// assert_eq!(Ordering::Less, finite.cmp(&infinity));
+/// assert_eq!(Ordering::Greater, finite.cmp(&negative_infinity));
+/// ```
+///
+/// # Versioning
+///
+/// Available since 1.0.0.
 impl<T> Ord for Infinitable<T>
 where
     T: Ord,
@@ -202,6 +265,27 @@ where
 {
     type Output = Infinitable<T::Output>;
 
+    /// Negates the value, when the underlying type `T` supports negation.
+    ///
+    /// `Infinity` is negated to `NegativeInfinity` (and vice versa),
+    /// and `Finite` is negated based on the underlying value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use infinitable::*;
+    ///
+    /// let finite = Finite(5);
+    /// assert_eq!(Finite(-5), -finite);
+    /// let infinity: Infinitable<i32> = Infinity;
+    /// assert_eq!(NegativeInfinity, -infinity);
+    /// let negative_infinity: Infinitable<i32> = NegativeInfinity;
+    /// assert_eq!(Infinity, -negative_infinity);
+    /// ```
+    ///
+    /// # Versioning
+    ///
+    /// Available since 1.3.0.
     fn neg(self) -> Infinitable<T::Output> {
         match self {
             Finite(x) => Finite(-x),
@@ -215,6 +299,27 @@ impl<T> Display for Infinitable<T>
 where
     T: Display,
 {
+    /// Formats the value, where the underlying type `T` supports formatting.
+    ///
+    /// `Infinity` is formatted to `"inf"`, `NegativeInfinity` is formatted to
+    /// `"-inf"`, and `Finite` is formatted based on the underlying value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use infinitable::*;
+    ///
+    /// let finite = Finite(5);
+    /// assert_eq!("5", format!("{}", finite));
+    /// let infinity: Infinitable<i32> = Infinity;
+    /// assert_eq!("inf", format!("{}", infinity));
+    /// let negative_infinity: Infinitable<i32> = NegativeInfinity;
+    /// assert_eq!("-inf", format!("{}", negative_infinity));
+    /// ```
+    ///
+    /// # Versioning
+    ///
+    /// Available since 1.2.0.
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
             Finite(x) => write!(f, "{}", x),
